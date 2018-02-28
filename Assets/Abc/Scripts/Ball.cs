b4ebe6bae41dce159901f3424fad7a0e46c02112
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2018-02-28 15:54:49 -0300

    - backbround

---
+++ Assets/Abc/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, 5);    
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Block"))
+        {
+            collision.GetComponent<Block>().ReceiveDamage(1);
+        }
+    }
+
+}
---
+++ Assets/Abc/Scripts/Block.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField] private int health;
+
+    public void ReceiveDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
